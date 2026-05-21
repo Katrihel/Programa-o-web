8144e8a45bb0046814db5700c3cb1fd8f2792b65
Katrihel <eduardo.delmondesr@gmail.com>
2026-05-21 12:43:34 -0300

    commit

---
+++ Atividade-2105/api.js
@@ -0,0 +1,13 @@
+const express = require('express'); ;; instancia do express
+
+const router = express.Router(); //modulariza as rotas
+
+const db = require('../db'); // conecta com o banco de dados
+
+//criar as rotas
+//cadastar o usuário
+//editar usuário
+//listar todos os usuarios
+//exlcuir o usuário
+
+module.exports = router;
